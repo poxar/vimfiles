7df62724fac07c80fb236c4d72e417177291f1a1
Philipp Millar <philipp.millar@gmx.de>
2012-09-25 11:14:43 +0200

    add scripts for bundle management

---
+++ bundle/b.bat
@@ -0,0 +1,25 @@
+@echo off
+cd %~dp0
+
+echo current path is %cd%
+echo.
+echo u - git pull for all bundles
+echo g - git clone for all bundles in bundles.txt
+echo * - abort
+echo.
+
+set /p choice="Enter your choice: "
+if "%choice%"=="u" goto update
+if "%choice%"=="g" goto get
+exit
+
+:update
+for /d %%X in ("*") do (
+        pushd %%X
+        git pull
+        popd)
+exit
+
+:get
+for /f "tokens=1" %%i in (bundles.txt) do git clone %%i
+exit
